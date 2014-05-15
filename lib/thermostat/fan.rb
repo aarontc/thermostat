6948e14788de84caf7bbd61cfcdc22d6a71625c8
Chris Haverkate <chris@techtonium.com>
2014-05-14 22:01:09 -0700

    Fan can be turned on and off

--- lib/thermostat/fan.rb
+++ lib/thermostat/fan.rb
@@ -4,9 +4,16 @@ class Fan
 	include AASM
 
 	aasm do
-		state :idle, initial: true
-		state :on
-		state :on_timed
+		state :idle, :initial => true
+		state :running
+		state :running_timed
 
+		event :start do
+			transitions :from => :idle, :to => :running
+		end
+
+		event :stop do
+			transitions :from => :running, :to => :idle
+		end
 	end
 end
--- test/test_fan.rb
+++ test/test_fan.rb
@@ -11,4 +11,21 @@ class TestFan < Minitest::Test
 		assert !@f.nil?
 	end
 
+	def test_default_fan_stat_is_idle
+		assert @f.idle?
+	end
+
+	def test_fan_can_transition_idle_to_on
+		@f.start
+		assert @f.running?
+	end
+
+	def test_a_fan_turned_on_can_return_to_idle
+		@f.start
+		assert @f.running?
+
+		@f.stop
+		assert @f.idle?
+	end
+
 end
